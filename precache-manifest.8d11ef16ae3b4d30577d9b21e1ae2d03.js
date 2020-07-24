564f259f5a34148ffc137f771c5c69b196128153
psihoz <psihozykt@gmail.com>
2020-07-24 19:16:48 +0300

    Updates

---
+++ precache-manifest.8d11ef16ae3b4d30577d9b21e1ae2d03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4edd0146b76e833070a81831a96e8a6b",
+    "url": "/obs/index.html"
+  },
+  {
+    "revision": "f9398e7b1fa94b0ce7a8",
+    "url": "/obs/static/js/2.b28ec9eb.chunk.js"
+  },
+  {
+    "revision": "e6191615acbaa88870aef4cd0368e4ec",
+    "url": "/obs/static/js/2.b28ec9eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6377a76232895b2896",
+    "url": "/obs/static/js/main.c723d0fd.chunk.js"
+  },
+  {
+    "revision": "ea1537d21da2e81dc278",
+    "url": "/obs/static/js/runtime-main.a06da743.js"
+  }
+]);
